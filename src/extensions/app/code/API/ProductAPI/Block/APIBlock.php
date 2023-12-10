c17e8ae2d476f8ae608ad923510f934fdfd89aee
Huy Nguyen <huynguyenquang030803@gmail.com>
2023-12-10 18:04:18 +0700

    update

--- src/extensions/app/code/API/ProductAPI/Block/APIBlock.php
+++ src/extensions/app/code/API/ProductAPI/Block/APIBlock.php
@@ -8,7 +8,7 @@ class APIBlock extends Template
 {
 	public function getText()
 	{
-		return "API modules collected by ManhDinh";
+		return "API modules collected by HuyNguyen";
 	}
 
 	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
